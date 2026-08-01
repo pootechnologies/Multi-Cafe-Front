54797b99f68e8b42cbe3bebfc48ec65898f3e841
Biruk Bizuayehu <birukbizuayehu1167@gmail.com>
2026-08-01 12:27:23 +0300

    package update

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"; // <-- add this
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      buffer: "buffer", // <-- alias for buffer module
+      buffer: "buffer",
     },
   },
   optimizeDeps: {
     esbuildOptions: {
       define: {
-        global: "globalThis", // <-- polyfill global
+        global: "globalThis",
       },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true, // <-- enable Buffer polyfill
-        }),
-      ],
     },
   },
 });
